068a1e9169322e29a473d3f6dfd21a78235a7d57
khasenovweb <hasenovkirill93@gmail.com>
2020-03-15 21:08:55 +0500

    dkfjdfjj

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,10 @@
- 
+ $(document).ready(function(){
+ 	$('.services__auto__circle').mouseover(function(){
+ 		var pos = $(this).offset();
+ 		var padding_right = $(window).width() - pos.left;
+ 		var otstup = 200 - padding_right;
+ 		if(padding_right < 200) {
+ 			$(this).children('.services__auto__circle__content').css('left', -otstup-60);
+ 		}
+ 	});
+ });
